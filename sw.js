be2e765ca85cb23570f4598cd7dbd7c0bcab33fd
DanSGrop1 <hello@groppiconsulting.com>
2026-06-13 10:34:47 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,5 @@
-const CACHE = 'burger-rater-v7';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(res => {
-        if (res && res.status === 200 && res.type === 'basic') {
-          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-        }
-        return res;
-      }).catch(() => e.request.mode === 'navigate' ? caches.match('./index.html') : undefined);
-    })
-  );
-});
+const CACHE = 'graze-unified-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(c => c || fetch(e.request).then(r => { if(r&&r.status===200&&r.type==='basic'){caches.open(CACHE).then(ca=>ca.put(e.request,r.clone()));} return r; }).catch(()=>e.request.mode==='navigate'?caches.match('./index.html'):undefined))); });
